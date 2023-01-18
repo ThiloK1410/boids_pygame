b9dbe330361c464a6e6611aa8b38beaa667e1776
thilo <thilokloos@gmx.de>
2023-01-18 14:10:59 +0100

    added module vector_manipulation

--- Actor.py
+++ Actor.py
@@ -1,4 +1,5 @@
 import pygame
+import vector_manipulation as vec
 import math
 
 
@@ -14,18 +15,11 @@ class Actor:
         self.mov = [dx, dy]
         self.size = [sz, sz]        # size should be quadratic
         self.screensize = [surface.get_width(), surface.get_height()]
-        self.mx_speed = 20
+        self.mx_speed = 5
         self.border_zone = 0.1
         self.border_push_force = 0.5      # TODO: maybe change border_zone or push_force in relation to mx_speed
         self.border_zone_abs = [int(self.screensize[0]*self.border_zone), int(self.screensize[1]*self.border_zone)]
         self.flocking_strength = f_s
-
-    @classmethod
-    def get_len(cls, direction):
-        temp = 0
-        for x in direction:
-            temp += x*x
-        return math.sqrt(temp)
 
     @classmethod
     def has_same_sign(cls, x, y):
@@ -55,28 +49,23 @@ class Actor:
             self.mov[1] -= self.border_push_force
 
     def set_max_speed(self, mx_speed):
-        speed = math.sqrt(self.get_len(self.mov))
+        speed = math.sqrt(vec.vector_length(self.mov))
         if speed > mx_speed:
             factor = mx_speed/speed
-            self.mov[0] *= factor
-            self.mov[1] *= factor
+            vec.multiply(self.mov, factor)
 
     def flock(self):
         if not self.flocking_strength == 0:
-            flock_dir = [0.0, 0.0]
-            flock_dir[0] = self.avg_pos[0] - self.pos[0]
-            flock_dir[1] = self.avg_pos[1] - self.pos[1]
-            if self.get_len(self.pos) > 50:
-                self.mov[0] += self.flocking_strength*flock_dir[0]
-                self.mov[1] += self.flocking_strength*flock_dir[1]
+            flock_dir = vec.get_direction(self.pos, self.avg_pos)
+            if vec.vector_length(flock_dir) > 50:
+                flock_dir = vec.multiply(flock_dir, self.flocking_strength)
+                self.mov = vec.add([self.mov, flock_dir])
 
     def collision(self):
         for x in self.population:
             if not x == self:
-                distance = [0.0, 0.0]
-                distance[0] = x.pos[0] - self.pos[0]
-                distance[1] = x.pos[1] - self.pos[1]
-                distance_abs = self.get_len(distance)
+                distance = vec.subtract([x.pos, self.pos])
+                distance_abs = vec.vector_length(distance)
                 if distance_abs < (self.size[0] + x.size[0])/2 and not distance_abs == 0:
                     if self.has_same_sign(self.mov[0], distance[0]):
                         self.mov[0] -= distance[0] / distance_abs
@@ -88,8 +77,7 @@ class Actor:
                         x.mov[1] += distance[1] / distance_abs
 
     def move(self):
-        self.pos[0] += self.mov[0]
-        self.pos[1] += self.mov[1]
+        self.pos = vec.add([self.pos, self.mov])
 
         # function which will be externally called every tick
     def on_step(self):
---
+++ vector_manipulation.py
@@ -0,0 +1,55 @@
+
+
+def vector_length(vector):
+    temp = float(0)
+    for x in vector:
+        temp += x*x
+    temp = temp**(1/float(len(vector)))
+    return temp
+
+
+def get_direction(start, end):
+    if not len(start) == len(end):
+        raise ValueError("get_direction function got Vectors of unequal length as input")
+    temp = []
+    for i, x in enumerate(start):
+        temp.append(end[i]-start[i])
+    return temp
+
+
+def add(vectors):
+    vec_len = None
+    for vector in vectors:
+        if vec_len is None:
+            vec_len = len(vector)
+        if not len(vector) == vec_len:
+            raise ValueError("add function got input vectors of unequal length")
+    temp = []
+    for i, x in enumerate(vectors[0]):
+        temp.append(0)
+        for vector in vectors:
+            temp[i] += vector[i]
+    return temp
+
+
+def subtract(vectors):
+    vec_len = None
+    for vector in vectors:
+        if vec_len is None:
+            vec_len = len(vector)
+        if not len(vector) == vec_len:
+            raise ValueError("subtract function got input vectors of unequal length")
+    temp = vectors[0].copy()
+    for i, vector in enumerate(vectors):
+        if i == 0:
+            continue
+        for j, x in enumerate(temp):
+            temp[j] -= vector[j]
+    return temp
+
+
+def multiply(vector, factor):
+    temp = []
+    for i, x in enumerate(vector):
+        temp.append(vector[i] * factor)
+    return temp
